b725c8fae4000b2996c5f86b2654f21f8023a12a
PiSera1 <kevinwellens24@gmail.com>
2025-01-23 12:10:46 +0100

    4. / Polymorph

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,25 +8,33 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();
 
-        output(queen.doYourJob());
-        output(queen.fly());
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+        // output(queen.doYourJob());
+        // output(queen.fly());
 
-        output(worker.doYourJob());
-        output(worker.fly());
+        // output(worker.doYourJob());
+        // output(worker.fly());
 
-        output(drone.doYourJob());
-        output(drone.fly());
+        // output(drone.doYourJob());
+        // output(drone.fly());
 
 
     }
 
-   
+    // Overloading --- gleiche M.-Name aber unterschiedliche P.-Struktur
+
+    private static void pollObj(HoneyBee obj){
+
+        output(obj.doYourJob());
+        output(obj.fly()); } {
+
+    }
    
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
-
 
 }
 
--- com/cc/java/Drone.java
+++ com/cc/java/Drone.java
@@ -3,7 +3,7 @@ package com.cc.java;
 public class Drone extends HoneyBee {
     
     public String doYourJob(){
-        return "I'm a worker bee, i work all day!";
+        return "I'm a drone, I'm going to date our Queen!";
     }
 
 }
